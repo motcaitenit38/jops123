de50df55ed0ec60839289d1f37ed3838e464f850
Lê Xuân Trường <xuantruong.220494@gmail.com>
2018-05-22 13:54:04 +0700

    404

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,25 +19,23 @@ class HomeController extends Controller
     public function home()
     {
         $tinh = Tinhthanhpho::all();
-        $nganhnghe = Nganhnghe::all();
-        return view('home',['tinh'=>$tinh,'nganhnghe'=>$nganhnghe]);
+        return view('home',['tinh'=>$tinh]);
     }
 
     public function search(Request $request){
-
+         $tinh = Tinhthanhpho::all();
         $abc = $request->search;
         $tp = $request->diachi;
         if($abc == ''){
 
             $congviec = Tuyendung_post::where('noilamviec',$tp)->orderBy('id','desc')->paginate(10);
-
             foreach ($congviec as $value) {
                 $abc = $value->tuyendung_user_id;
             }
 
-            $tencongty = Tuyendung_info::where('tuyendung_id',$abc)->first();
+            $tencongty = Tuyendung_info::where('tuyendung_id',$abc)->firstOrFail();
             $tencongty = $tencongty->tencongty;
-            return view('timkiem.ketqua',['congviecs'=>$congviec,'tencongty'=>$tencongty]);
+            return view('timkiem.ketqua',['congviecs'=>$congviec,'tencongty'=>$tencongty,'tinh'=>$tinh]);
         }
         else{
             $congviec = Tuyendung_post::where('tencongviec','like', '%'.$abc.'%')
@@ -50,12 +48,12 @@ class HomeController extends Controller
 
             $tencongty = Tuyendung_info::where('tuyendung_id',$abc)->first();
             $tencongty = $tencongty->tencongty;
-            return view('timkiem.ketqua',['congviecs'=>$congviec,'tencongty'=>$tencongty]);
+            return view('timkiem.ketqua',['congviecs'=>$congviec,'tencongty'=>$tencongty,'tinh'=>$tinh]);
         }
     }
 
     public function chitiet($id){
-        $chitiet = Tuyendung_post::where('id',$id)->first();
+        $chitiet = Tuyendung_post::where('id',$id)->firstOrFail();
         $info = Tuyendung_info::where('tuyendung_id', $chitiet->tuyendung_user_id)->first();
         return view('timkiem.chitiet-congviec',['chitiet'=>$chitiet,'thongtincongty'=>$info]);
     }
